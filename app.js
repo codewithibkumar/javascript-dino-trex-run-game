5e211611e517e1e87b12626fd51b7a9252c554d5
ibkumar <ibkumarandroid@gmail.com>
2024-06-28 22:04:36 +0530

    Added jump feature

--- app.js
+++ app.js
@@ -1,1 +1,50 @@
-console.log("javascript running");
+document.addEventListener('DOMContentLoaded',()=>{
+    const dino = document.querySelector(".dino");
+    const grid = document.querySelector(".grid");
+    let gravity = 0.9;
+    let isJumping = false;
+
+    function control(e){
+        if(e.keyCode === 32){
+            
+            if(!isJumping){
+                console.log("jump");
+                jump();
+            }
+           
+        }
+
+        // if(e.code === "Space"){
+        //     console.log("jump");
+        //     jump();
+        // }
+    }
+    let position = 0;
+    function jump(){
+        isJumping = true;
+     let count = 0;
+     let timerId = setInterval(function(){
+        //move down
+        if(count === 15){
+            clearInterval(timerId);
+            let downTimerId = setInterval(function(){
+                // console.log("going down")
+                if(count == 0){
+                    clearInterval(downTimerId);
+                    isJumping = false;
+                }
+                position -= 5;
+                count--;
+                position = position * gravity;
+                dino.style.bottom = position +"px";
+            },20)
+        }
+        //move up
+        position +=30;
+        count++;
+        position = position * gravity;
+        dino.style.bottom = position + "px";
+     },20)
+    }
+     document.addEventListener('keydown',control);
+})
